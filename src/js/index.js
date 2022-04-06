728462edcd9d8870ad571ff569859001b68eb50e
Vladimir Shepel <web.shepel@gmail.com>
2022-04-06 15:48:34 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Swiper, { Mousewheel, Pagination } from "swiper";
 import * as LottiePlayer from "@lottiefiles/lottie-player";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin.js";
 
 // Swiper products
 (function() {
@@ -76,11 +77,11 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 // Parallax
 gsap.registerPlugin(ScrollTrigger);
 
-(function () {
-  const aboutEl = document.querySelector('#about');
+(function() {
+  const aboutEl = document.querySelector("#about");
 
   if (!aboutEl)
-    return
+    return;
 
   gsap.to("#about-item-1 .about-item__content", {
     yPercent: -15,
@@ -88,7 +89,7 @@ gsap.registerPlugin(ScrollTrigger);
     scrollTrigger: {
       trigger: "#about",
       scrub: 1.5
-    },
+    }
   });
 
   gsap.to("#about-item-2 .about-item__content", {
@@ -97,7 +98,7 @@ gsap.registerPlugin(ScrollTrigger);
     scrollTrigger: {
       trigger: "#about",
       scrub: 1.5
-    },
+    }
   });
 
   gsap.to(".product-item__shape", {
@@ -106,6 +107,40 @@ gsap.registerPlugin(ScrollTrigger);
     scrollTrigger: {
       trigger: "#products",
       scrub: 1.2
-    },
+    }
   });
+})();
+
+// Scroll to
+gsap.registerPlugin(ScrollToPlugin);
+
+(function() {
+  function getSamePageAnchor(link) {
+    if (
+      link.protocol !== window.location.protocol ||
+      link.host !== window.location.host ||
+      link.pathname !== window.location.pathname ||
+      link.search !== window.location.search
+    ) {
+      return false;
+    }
+
+    return link.hash;
+  }
+
+  function scrollToHash(hash, e) {
+    const elem = hash ? document.querySelector(hash) : false;
+    if (elem) {
+      if (e) e.preventDefault();
+      gsap.to(window, { duration: 1.5, scrollTo: elem, ease: "power2" });
+    }
+  }
+
+  document.querySelectorAll("a[href]").forEach(a => {
+    a.addEventListener("click", e => {
+      scrollToHash(getSamePageAnchor(a), e);
+    });
+  });
+
+  scrollToHash(window.location.hash);
 })();
